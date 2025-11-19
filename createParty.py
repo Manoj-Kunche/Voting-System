b2807c61ba29947f654439c3f0c55adb5d9a5855
SreeCharan153 <223j1a4632@raghuinstech.com>
2025-11-19 18:33:23 +0530

    Add party creation functionality to the Voting System

---
+++ createParty.py
@@ -0,0 +1,16 @@
+class addparty:
+    def __init__(self, party_name, party_symbol):
+        self.party_name = party_name
+
+
+    def get_party_details(self):
+        return {
+            "party_name": self.party_name
+        }
+    
+    def create_party(self):
+        with open("./partys/parties.txt", "a") as file:
+            file.write(f"{self.party_name}\n")
+        with open(f"./partys/{self.party_name}.txt", "w") as file:
+            file.write(f"Votes: {self.party_name} : 0\n")
+        return f"Party '{self.party_name}' created successfully."
